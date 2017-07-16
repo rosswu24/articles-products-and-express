8cb6611601f76bfe8550ad8a50c1b4298eb1e7df
Dory Baga <dorybaga@Dorys-MacBook-Pro.local>
2017-07-15 19:46:56 -1000

    handles post req

--- routes/products.js
+++ routes/products.js
@@ -1,19 +1,12 @@
 /*jshint esversion: 6*/
-
 const express = require('express');
 const router = express.Router();
 
 router.route('/')
-	.get((req,res)=>{
-		console.log("pinged the route")
-		res.end();
-	});
 
 
-router.route('/routes')
-	.get((req,res) => {
-		console.log('got the yummy food');
-		res.end();
-	});
+  .post((req, res) => {
+    console.log("this is the object: ", req.body);
+  });
 
 	module.exports = router;
