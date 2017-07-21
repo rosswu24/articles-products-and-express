03c1467a7293cf881f8331a5552eab195dc00f41
Dory Baga <dorybaga@Dorys-MacBook-Pro.local>
2017-07-20 18:16:05 -1000

    added hbs engine and added comments

--- server.js
+++ server.js
@@ -11,23 +11,33 @@ const app = express();
 // const articleRoute = require('./routes/articles.js');
 const productsRoute = require('./routes/products.js');
 
-
 const PORT = process.env.PORT || 3000;
 
+// create handlebars instance and store in var
+const hbs = exphbs.create({
+  extname: '.hbs',
+  defaultLayout: 'main'
+});
+
+// invoke exphbs; takes in obj configuration
+app.engine('hbs', hbs.engine);
+
+// use the handlebars engine
+app.set('view engine', 'hbs');
 app.use(bodyParser.json());
 
 // app.use('/routes', articleRoute);
 app.use('/products', productsRoute);
 
-// app.get('', (req, res) => {
-//   console.log("this is the name of the product: ", req);
-// });
+app.get('', (req, res) => {
+  console.log("this is the name of the product: ", req);
+});
 
 app.get('/products/:id', (req, res) => {
   console.log("ID: " + req.params.id);
 });
 
-
+// make server listen for data
 const server = app.listen(PORT, () => {
 	console.log(`server running on ${PORT}`);
 });
