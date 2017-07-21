f080a817a6ae686a91803ef7fec9948ee4f729fe
Dory Baga <dorybaga@Dorys-MacBook-Pro.local>
2017-07-20 22:02:22 -1000

    added deleteById(); is working

--- db/products.js
+++ db/products.js
@@ -11,20 +11,24 @@ function getProductById(itemId){
   for (var i = 0; i < inventory.length; i++) {
     console.log("inventory id:", inventory[i].id, "item id: ", itemId);
     console.log("inventory id:", typeof inventory[i].id, "item id: ", typeof itemId);
-      if (inventory[i].id === itemId) {
-          console.log("found item: ", inventory[i]);
-          return inventory[i];
-      }
+    if (inventory[i].id === itemId) {
+        console.log("found item: ", inventory[i]);
+        return inventory[i];
+    }
   }
-
-  // inventory.findIndex( x => x.id === itemId);
-  // console.log('found item id: ' + itemId);
-  // want to return whole obj
-
 }
 
-function deleteById(id){
-
+function deleteById(itemId){
+  for (var i = 0; i < inventory.length; i++) {
+    if (inventory[i].id === itemId) {
+      console.log("inventory id:", inventory[i].id, "item id: ", itemId);
+      console.log("item to delete:", inventory[i]);
+        var itemIdx = inventory.indexOf(inventory[i]);
+        inventory.splice(itemIdx, 1);
+        console.log(inventory);
+        return inventory;
+    }
+  }
 }
 
 function createNewProduct(product){
