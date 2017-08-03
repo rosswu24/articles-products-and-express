44f8d46100aa03eed77780e1882c024a455cef53
Dory <dorybaga@gmail.com>
2017-08-02 21:46:50 -1000

    added editById()

--- db/products.js
+++ db/products.js
@@ -18,6 +18,19 @@ function getProductById(itemId){
   }
 }
 
+function editById(itemId, newName, newPrice, newCount){
+  for (var i = 0; i < inventory.length; i++) {
+    if (inventory[i].id === itemId) {
+      console.log("inventory id:", inventory[i].id, "item id: ", itemId);
+      console.log("item to edit:", inventory[i]);
+        var itemIdx = inventory.indexOf(inventory[i]);
+        inventory[itemIdx] = `{name: ${newName}, price: ${newPrice}, inventory: ${newCount}, id: itemId}`;
+        console(inventory);
+        return inventory;
+    }
+  }
+}
+
 function deleteById(itemId){
   for (var i = 0; i < inventory.length; i++) {
     if (inventory[i].id === itemId) {
