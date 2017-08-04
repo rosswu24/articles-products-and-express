ed516c213267dbbd334c02ed52a47b18c2b7d264
Dory <dorybaga@gmail.com>
2017-08-03 21:34:31 -1000

    added route for products/new; renders new inventory form; submit button adds item to new inventory; rerouted back to /products

--- routes/products.js
+++ routes/products.js
@@ -4,11 +4,15 @@ const router = express.Router();
 const DB_products = require('../db/products.js');
 const app = express();
 
+router.route('/new')
+  .get((req, res) => {
+  res.render('products/new');
+  });
+
 router.route('/')
   .post((req, res) => {
 	DB_products.createNewProduct(req.body);
-  res.render('products/new');
-	res.end();
+  res.redirect('/products');
 	})
 
   .get((req,res) => {
